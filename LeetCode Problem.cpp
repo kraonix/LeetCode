29185a9c25cf604818046e8c253affdf61a296f4
Sachin <104882037+kraonix@users.noreply.github.com>
2025-11-15 23:42:34 +0530

    Add LeetCode Problem.cpp

---
+++ LeetCode Problem.cpp
@@ -0,0 +1,20 @@
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        double result = 0.0;
+
+        for(int i = 0; i<nums2.size(); i++){
+            nums1.push_back(nums2[i]);
+        }
+
+        sort(nums1.begin(), nums1.end());
+
+        if(nums1.size()%2 != 0){
+            result = nums1[(nums1.size()/2)];
+            return result;
+        }else{
+            result = (nums1[(nums1.size()/2)] + nums1[(nums1.size()/2)-1])/2.0;
+            return result;
+        }
+    }
+};
+class Solution {
