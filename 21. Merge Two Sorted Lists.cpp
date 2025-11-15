a114539a89429fb3b7a9a7bd0f1b0a2de0514b45
Sachin <104882037+kraonix@users.noreply.github.com>
2025-11-16 00:13:24 +0530

    Append submission to 21. Merge Two Sorted Lists.cpp

--- 21. Merge Two Sorted Lists.cpp
+++ 21. Merge Two Sorted Lists.cpp
@@ -1,4 +1,29 @@
 /**
+Â *Â DefinitionÂ forÂ singly-linkedÂ list.
+Â *Â structÂ ListNodeÂ {
+Â *Â Â Â Â Â intÂ val;
+Â *Â Â Â Â Â ListNodeÂ *next;
+Â *Â Â Â Â Â ListNode()Â :Â val(0),Â next(nullptr)Â {}
+Â *Â Â Â Â Â ListNode(intÂ x)Â :Â val(x),Â next(nullptr)Â {}
+Â *Â Â Â Â Â ListNode(intÂ x,Â ListNodeÂ *next)Â :Â val(x),Â next(next)Â {}
+Â *Â };
+Â */
+classÂ SolutionÂ {
+public:
+Â Â Â Â ListNode*Â mergeTwoLists(ListNode*Â list1,Â ListNode*Â list2)Â {
+Â Â Â Â Â Â Â Â 
+Â Â Â Â Â Â Â Â if(list1Â ==Â NULLÂ ||Â list2Â ==Â NULL){
+Â Â Â Â Â Â Â Â Â Â Â Â returnÂ list1Â ==Â NULLÂ ?Â list2Â :Â list1;
+Â Â Â Â Â Â Â Â }
+
+Â Â Â Â Â Â Â Â if(list1->valÂ <=Â list2->val){
+Â Â Â Â Â Â Â Â Â Â Â Â list1->nextÂ =Â mergeTwoLists(list1->next,Â list2);
+Â Â Â Â Â Â Â Â Â Â Â Â returnÂ list1;
+Â Â Â Â Â Â Â Â }else{
+
+
+// --- New Submission ---
+/**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
