203e3538b3f3b060eb7f272963b0a45853ec084e
Sachin <104882037+kraonix@users.noreply.github.com>
2025-12-25 21:04:02 +0530

    Add 3783. Mirror Distance of an Integer.cpp

---
+++ 3783. Mirror Distance of an Integer.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int mirrorDistance(int n) {
+        string temp = to_string(n);
+        reverse(temp.begin(), temp.end());
+        int a = stoi(temp);
+        return abs(n - a);
+    }
+};
