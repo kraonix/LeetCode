75da61942968ca0c7936187904cd967ba6e41f1f
Sachin <104882037+kraonix@users.noreply.github.com>
2026-02-10 12:25:07 +0530

    Append submission to LeetCode Problem.cpp

--- LeetCode Problem.cpp
+++ LeetCode Problem.cpp
@@ -25,3 +25,24 @@ public:
         }
     }
 };
+
+// --- New Submission ---
+public class Solution {
+    public int ScoreOfString(string s) {
+        
+        Dictionary<char, int> alpha = new();
+        
+        int j = 97;
+        for(char c = 'a'; c <= 'z'; c++){
+            alpha[c] = j;
+            j++;
+        }
+
+        int result = 0;
+        for(int i = 0; i < s.Length - 1; i++){
+            result += Math.Abs(alpha[s[i]] - alpha[s[i + 1]]);
+        }
+
+        return result;
+    }
+}
