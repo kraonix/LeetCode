5410d8ad06617bd999d3c52774fba0cc506bfe41
Sachin <104882037+kraonix@users.noreply.github.com>
2026-02-22 15:29:33 +0530

    Append submission to 2942. Find Words Containing Character.cpp

--- 2942. Find Words Containing Character.cpp
+++ 2942. Find Words Containing Character.cpp
@@ -12,3 +12,19 @@ public:
         return result;
     }
 };
+
+// --- New Submission ---
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> result;
+        for(int i = 0; i < words.size(); i++){
+            set<char> temp(words[i].begin(), words[i].end());
+            if(temp.count(x)){
+                result.push_back(i);
+            }
+        }
+
+        return result;
+    }
+};
